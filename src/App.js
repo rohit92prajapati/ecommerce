b912e8dfc3942ef044bef6fb4b761e8e81570846
rohit92prajapati <rohit92prajapati@gmail.com>
2022-12-01 22:40:56 +0530

    Initial setup

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import ProductPage from './component/ProductPage';
+function App() {
+  return (
+    <div>
+      <ProductPage/>
+    </div>
+  );
+}
+
+export default App;
